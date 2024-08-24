ea0cf7417fe29933f933649d34221c9acbbca84b
Olaniyi Muiez Kolawole <86967393+Kolawole26@users.noreply.github.com>
2024-08-24 17:59:48 +0100

    quick fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
       <header className='h-screen flex justify-center px-40'>
               <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat animate-zoom"
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat animate-zoom overflow-hidden"
         style={{ backgroundImage: "url('/images/hero.jpg')" }}
       />
         <div className="absolute inset-0 bg-black bg-opacity-50" />
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,14 +1,18 @@
 import SEO from '../components/SEO';
-import Layout from '../components/Layout';
+import Navigation from '../components/Navigation';
 
 export default function Menu() {
   return (
     <>
       <SEO title="Menu Page" description="Welcome to the homepage of My Website, where you can find amazing content." />
-      <Layout>
-      <h1>Welcome to My Website</h1>
-      <p>This is the home page. Here you can find all the latest updates and information about our services.</p>
-      </Layout>
+      <section className='px-40'>
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat scale-150"
+        style={{ backgroundImage: "url('/images/hero.jpg')" }}
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50" />
+      <Navigation backgroundColor="bg-white"/>
+      </section>
     </>
   );
 }
